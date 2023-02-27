177a013f7d53a0f776db6e1fdb9721f086cd84fc
Hamdaan Khalid <khalid.hamdaan@gmail.com>
2023-02-26 20:25:56 -0800

    make sense of sln

--- GraphyBackend.UnitTests/ItemsControllerTests.cs
+++ GraphyBackend.UnitTests/ItemsControllerTests.cs
@@ -5,26 +5,25 @@ using GraphyBackend.Api.Repositories;
 using System;
 using GraphyBackend.Api.Models;
 using System.Threading.Tasks;
-using GraphyBackend.Api.Dtos;
 using GraphyBackend.Api.Controllers;
 using Microsoft.AspNetCore.Mvc;
 
 
 namespace GraphyBackend.UnitTests
 {
-	
+
     public class ItemsControllerTests
     {
 
         private readonly Mock<IItemsRepository> repositoryStub = new Mock<IItemsRepository>();
-		
-		private readonly Random rand = new Random();
+
+        private readonly Random rand = new Random();
 
         [Fact]
         public async Task GetItem_WithUnexistingItem_ReturnsNotFound()
         {
             // Arrange
-            repositoryStub.Setup(repo => repo.GetItem(It.IsAny<Guid>())).ReturnsAsync((Item)null);
+			repositoryStub.Setup(repo => repo.GetItem(It.IsAny<Guid>())).ReturnsAsync((Item)null);
             var controller = new ItemsController(repositoryStub.Object);
 
             // Act
@@ -32,14 +31,14 @@ namespace GraphyBackend.UnitTests
             var result = await controller.GetItem(Guid.NewGuid());
 
             // Assert
-			result.Result.Should().BeOfType<NotFoundResult>();
-		}
+            result.Result.Should().BeOfType<NotFoundResult>();
+        }
 
         [Fact]
         public async Task GetItem_WithExistingItem_ReturnsExpectedItem()
         {
             // Arrange
-			var expectedItem = CreateRandomItem();
+            var expectedItem = CreateRandomItem();
             repositoryStub.Setup(repo => repo.GetItem(It.IsAny<Guid>())).ReturnsAsync(expectedItem);
             var controller = new ItemsController(repositoryStub.Object);
 
@@ -48,22 +47,22 @@ namespace GraphyBackend.UnitTests
             var result = await controller.GetItem(Guid.NewGuid());
 
             // Assert
-			result.Value.Should().BeEquivalentTo(
-					expectedItem,
-					options => options.ComparingByMembers<Item>()
-			);
-		}
+            result.Value.Should().BeEquivalentTo(
+                    expectedItem,
+                    options => options.ComparingByMembers<Item>()
+            );
+        }
 
-		private Item CreateRandomItem() 
-		{
-			return new Item
-			{
-				Id = Guid.NewGuid(),
-				Name = Guid.NewGuid().ToString(),
-				Price = rand.Next(1000),
-				CreatedDate = DateTimeOffset.UtcNow
-			};
-		}
+        private Item CreateRandomItem()
+        {
+            return new Item
+            {
+                Id = Guid.NewGuid(),
+                Name = Guid.NewGuid().ToString(),
+                Price = rand.Next(1000),
+                CreatedDate = DateTimeOffset.UtcNow
+            };
+        }
     }
 
 }
